b90e23a9d614cb8c5fb151687f5c44d5c67a63a5
DEVAANSH001 <theman3185@gmail.com>
2026-01-11 19:49:34 +0530

    completed sidebar with user context

--- app/dashboard/Provider.tsx
+++ app/dashboard/Provider.tsx
@@ -1,14 +1,17 @@
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
 import React from 'react'
 import { AppSidebar } from './_component/AppSidebar'
+import AppHeader from './_component/AppHeader'
 
 const DashboardProvider = ({children}: {children: React.ReactNode}) => {
   return (
     <div>
         <SidebarProvider>
             <AppSidebar />
-    <SidebarTrigger />
-        {children}
+            <div className ="w-full">
+            <AppHeader/>
+          {children}
+            </div>
     </SidebarProvider>
     </div>
   )
--- app/dashboard/_component/AppSidebar.tsx
+++ app/dashboard/_component/AppSidebar.tsx
@@ -1,35 +1,109 @@
-import Image from "next/image"
+"use client";
+import Image from "next/image";
+import Link from "next/link";
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
   SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
   SidebarHeader,
-} from "@/components/ui/sidebar"
-import { LayoutDashboard } from "lucide-react"
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "@/components/ui/sidebar";
+import {
+  Database,
+  Gem,
+  Headphones,
+  LayoutDashboard,
+  User,
+  WalletCards,
+} from "lucide-react";
+import { useContext } from "react";
+import { UserDetails } from "@/context/UserData";
+import { Button } from "@/components/ui/button";
+import { usePathname } from "next/navigation";
 
-const MenuOptions=[
-    {
-        title:"Dashboard",
-        url:"/dashboard",
-        icon:LayoutDashboard
-    }
-]
+const MenuOptions = [
+  {
+    title: "Dashboard",
+    url: "/dashboard",
+    icon: LayoutDashboard,
+  },
+  {
+    title: "AI Agents",
+    url: "#",
+    icon: Headphones,
+  },
+  {
+    title: "Data",
+    url: "#",
+    icon: Database,
+  },
+  {
+    title: "Pricing",
+    url: "#",
+    icon: WalletCards,
+  },
+  {
+    title: "Profile",
+    url: "#",
+    icon: User,
+  },
+];
 
 export function AppSidebar() {
+  //check for sidebar open or not
+  const { open } = useSidebar();
+  const { userDetails, setUserDetail } = useContext(UserDetails);
+  // console.log("UserDetails from context:", userDetails);
+  const path = usePathname()
+
   return (
-    <Sidebar>
+    <Sidebar collapsible="icon">
       <SidebarHeader>
         <div className="flex items-center gap-2">
-        <Image src={'/logo.svg'} alt="Logo" width={35} height={35} />
-        <h2 className="text-lg font-bold">AgentCanvas</h2>
+          <Image src={"/logo.svg"} alt="Logo" width={35} height={35} />
+          {open && <h2 className="text-lg font-bold">AgentCanvas</h2>}
         </div>
-        </SidebarHeader>
+      </SidebarHeader>
       <SidebarContent>
-        <SidebarGroup />
-        <SidebarGroup />
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {MenuOptions.map((menu, index) => (
+                <SidebarMenuItem key={index}>
+                  <SidebarMenuButton asChild size={open ? "lg" : "default"} isActive={path===menu.url}>
+                    <Link href={menu.url}>
+                      <menu.icon />
+                      <span>{menu.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
       </SidebarContent>
-      <SidebarFooter />
+      <SidebarFooter className="mb-5">
+        <div className="flex items-center gap-2">
+          <Gem />
+          {open && (
+            <h2 className="text-sm font-medium">
+              Remaining Credit: 
+              <span className="font-bold">{userDetails?.token ?? 0}</span>
+            </h2>
+          )}
+        </div>
+        
+
+          {open &&<Button>Upgrade to Premium</Button>}
+        
+      </SidebarFooter>
     </Sidebar>
-  )
+  );
 }
